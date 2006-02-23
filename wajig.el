3769f26cbc3c0c1f19a2364b2a862c132adb7f03
william.xwl <william.xwl@gmail.com>
2006-02-23 16:29:00 +0000

    1. Distinguish between general wajig commands and those for controlling
       system services. (Mainly they differ in completing list.)
    
    2. Add output highlight support.
    
    3. Replace `delete-process' with `kill-process' to make `wajig-kill'
       actually work.
    
    4. Upgrade version to 0.3. (Now we've accomplished most of the stuffs as
       far as i can see.)
    
    darcs-hash:20060223162952-e8fe6-de3e7d2168648f94a64955074c117f37e80e4533.gz

--- wajig.el
+++ wajig.el
@@ -1,9 +1,9 @@
 ;;; wajig.el --- an interface for wajig
 
-;; Copyright (C) 2005 William Xu
+;; Copyright (C) 2005, 2006 William Xu
 
 ;; Author: William Xu <william.xwl@gmail.com>
-;; $Id: wajig.el,v 0.2 2006/01/20 22:52:31 xwl Exp $
+;; $Id: wajig.el,v 0.3 2006/02/24 00:22:39 xwl Exp $
 
 ;; This file is not part of GNU Emacs.
 
@@ -38,6 +38,23 @@
 ;;
 ;; Then, simply run `M-x wajig'.
 
+;; Features
+;; --------
+
+;; - Ease of Typing
+
+;;   Most of wajig commands can be invoked by typing very few
+;;   keys. e.g., in *wajig* buffer, `o' for `wajig show'.
+
+;; - `ido-completing-read' Support
+
+;;   With the power of `ido-completing-read', you don't have to remember
+;;   the full name of a package. It is also an ease of typing.
+
+;; - Output Highlighting
+
+;;   Highlight command outputs. Make them look nicer.
+
 ;; The Aim
 ;; -------
 
@@ -50,12 +67,15 @@
 ;; Important Notes
 ;; ---------------
 
-;; 1. It's expected to run all the commands in a non-interactive
-;; style. So do set APT::GET::Assume-Yes to "true".
+;; 1. It's currently expected to run all the commands in a
+;;    non-interactive style. So do have the following settings:
+;;
+;;        - set APT::GET::Assume-Yes to "true".
+;;        - dpkg-reconfigure debconf, and choose non-interactive.
 
 ;; 2. I only tested wajig commands that i tend to use often. Far from
-;; all of the wajig commands! Hence, possibly some functions won't work
-;; properly. Be careful and let me know if you find any!
+;;    all of the wajig commands! Hence, possibly some functions won't
+;;    work properly. Be careful and let me know if you find any!
 
 ;;; Change Log:
 
@@ -63,9 +83,8 @@
 
 ;;; Todo:
 
-;; 5. hightlight keywords in outputs?
-;; 6. fix the output of `wajig udpate'.
-;; 7. re-cache for update installed pkgs seems not working?
+;; - Make output with percentage number(e.g., `wajig update') display
+;;   nicer.
 
 ;;; Code:
 
@@ -96,7 +115,7 @@
   "Wajig command process.")
 
 (defvar wajig-running nil
-  "Is wajig process running?")
+  "Is the wajig process running?")
 
 (defun wajig-update-installed-pkgs ()
   "Update `wajig-installed-pkgs'."
@@ -109,6 +128,24 @@
   (wajig-update-installed-pkgs)
   "Installed packages on the system. You can run
 `wajig-update-installed-pkgs' to keep update.")
+
+(defun wajig-update-daemons ()
+  "Update daemons' list."
+  (interactive)
+  (setq wajig-daemons
+	(split-string
+	 (replace-regexp-in-string
+	  "^Found.*\n" ""
+	  (shell-command-to-string "sudo wajig list-daemons")))))
+
+(defvar wajig-daemons
+  (wajig-update-daemons)
+  "Existing daemons' list. You can run `wajig-update-daemons' to
+keep update.")
+
+(defvar wajig-daemons-command-list
+  '(wajig-start wajig-stop wajig-restart wajig-reload)
+  "Wajig command list for controling system daemons.")
 
 (defun wajig-process-sentinel (process event)
   "Set buffer read-only after a wajig command finishes."
@@ -127,25 +164,33 @@
   "Filter wajig command outputs."
   (with-current-buffer (process-buffer process)
     (let ((moving (= (point) (process-mark process)))
-	  (inhibit-read-only t))
+	  (inhibit-read-only t)
+	  (percentage-match "[0-9]\\{1,2\\}%"))
       (save-excursion
 	(goto-char (process-mark process))
-	(insert (replace-regexp-in-string "" "\n" output))
-	;; (insert output)
+	(setq output (replace-regexp-in-string "" "\n" output))
+	;; make percentage output nicer.
+;; 	(when (zerop (forward-line -1))
+;; 	  (if (string-match (concat "^" percentage-match "%") output)
+;; 	      (progn
+;; 		(kill-line)
+;; 		(kill-line))
+;; 	    (forward-line 1)))
+ 	(insert output)
 	(set-marker (process-mark process) (point)))
       (if moving (goto-char (process-mark process))))))
 
 (defun wajig-kill ()
-  "Kill wajing process."
+  "Kill wajig process."
   (interactive)
   (when wajig-process
     (unless (eq (process-status wajig-process) 'exit)
-      (kill-process wajig-process)
+      (delete-process wajig-process)
       (setq wajig-running nil))))
 
 (defmacro define-wajig-command (command &optional arglist)
   "Define a new wajig command. COMMAND is one of wajig commands,
-such as help, update. ARGLIST is (pkg). e.g.,
+such as help, update. Optional ARGLIST is (pkg). e.g.,
 
     (define-wajig-command help)
     (define-wajig-command show (pkg))
@@ -153,50 +198,45 @@ such as help, update. ARGLIST is (pkg). e.g.,
 pkg is the package name to operate on."
   (when (symbolp command)
     (setq command (symbol-name command)))
-  (let ((wajig-command (intern (format "wajig-%s" command)))
-	(wajig-arglist) (docstring) (interactive))
-    (if arglist
-	(setq wajig-arglist (append arglist '(&optional re-cache))
-	      docstring (progn
-			  (string-match
-			   (format "^ %s.*" command) wajig-commands-string)
-			  (concat
-			   (match-string 0 wajig-commands-string)
-			   "\n\nNon-nil RE-CACHE forces re-caching `wajig-installed-pkgs'."))
-	      interactive `(interactive
-			    (list (wajig-completing-read ; require emacs22.
-				   ,(format "$ sudo wajig %s " command)
-				   wajig-installed-pkgs))))
-      (setq wajig-arglist nil	       ; show help from `wajig commands'
-	    docstring (progn
-			(string-match
-			 (format "^ %s.*" command) wajig-commands-string)
-			(match-string 0 wajig-commands-string))
-	    interactive '(interactive)))
-    `(progn
-       (defun ,wajig-command ,wajig-arglist
-	 ,docstring
-	 ,interactive
-	 (let ((inhibit-read-only t))
-	   (wajig)
-	   (erase-buffer)
-	   (if wajig-running
-	       (error "Wajig process already exists")
-	     (setq wajig-running t)
-	     (setq wajig-process
-		   ,(if arglist
-			`(progn
-			   (when re-cache
-			     (wajig-update-installed-pkgs))
-			   (start-process "wajig" "*wajig*"
-					  "sudo" "wajig" ,command ,(car arglist)))
+  (let* ((wajig-command (intern (format "wajig-%s" command)))
+	 (docstring
+	  (progn		       ; show help from `wajig commands'
+	    (string-match (format "^ %s.*" command)
+			  wajig-commands-string)
+	    (match-string 0 wajig-commands-string)))
+	 (interactive
+	  (if arglist
+	      (setq interactive
+		    `(interactive
+		      (list
+		       (wajig-completing-read
+			,(format "$ sudo wajig %s " command)
+			,(if (memq wajig-command
+				  wajig-daemons-command-list)
+			    'wajig-daemons
+			  'wajig-installed-pkgs)))))
+	    '(interactive))))
+    `(defun ,wajig-command ,arglist
+       ,docstring
+       ,interactive
+       (let ((inhibit-read-only t))
+	 (wajig)
+	 (erase-buffer)
+	 (if wajig-running
+	     (error "Wajig process already exists")
+	   (setq wajig-running t)
+	   (setq wajig-process
+		 ,(if arglist
 		      `(start-process "wajig" "*wajig*"
-				      "sudo" "wajig" ,command)))
-	     (set-process-filter wajig-process 'wajig-process-filter)
-	     (set-process-sentinel wajig-process 'wajig-process-sentinel)))))))
+				      "sudo" "wajig" ,command ,(car arglist))
+		    `(start-process "wajig" "*wajig*"
+				    "sudo" "wajig" ,command)))
+	   (set-process-filter wajig-process 'wajig-process-filter)
+	   (set-process-sentinel wajig-process 'wajig-process-sentinel))))))
 
 (defun wajig-do (command-string)
-  "Run COMMAND-STRING in *wajig* buffer."
+  "Run COMMAND-STRING list(e.g., '(\"cmd\" \"arg1\" ...)) in *wajig*
+buffer."
   (let ((inhibit-read-only t))
     (wajig)
     (erase-buffer)
@@ -223,52 +263,16 @@ pkg is the package name to operate on."
  (lambda (command)
    ;; FIXME: Is this the correct way, using `eval'?
    (eval `(define-wajig-command ,command)))
- '(auto-clean
-   auto-download
-   clean
-   commands
-   daily-upgrade
-   describe-new
-   detail-new
-   dist-upgrade
-   docs
-   file-download
-   file-remove
-   fix-configure
-   fix-install
-   fix-missing
-   init
-   integrity
-   help
-   large
-   last-update
-   list
-   list-all
-   list-cache
-   list-commands
-   list-daemons
-   list-hold
-   list-installed
-   list-log
-   list-orphans
-   local-dist-upgrade
-   local-upgrade
-   non-free
-   orphans
-   policy
-   reset
-   search-apt
-   setup
-   showdistupgrade
-   showinstall
-   showremove
-   showupgrade
-   size
-   sizes
-   snapshot
-   tasksel
-   toupgrade
-   update))
+ '(
+   auto-clean auto-download clean commands daily-upgrade
+   describe-new detail-new dist-upgrade docs file-download
+   file-remove fix-configure fix-install fix-missing init integrity
+   help large last-update list list-all list-cache list-commands
+   list-daemons list-hold list-installed list-log list-orphans
+   local-dist-upgrade local-upgrade non-free orphans policy reset
+   search-apt setup showdistupgrade showinstall showremove
+   showupgrade size sizes snapshot tasksel toupgrade update
+   ))
 
 ;; Wajig Command With One Argument
 ;; -------------------------------
@@ -276,67 +280,19 @@ pkg is the package name to operate on."
 (mapc
  (lambda (command)
    (eval `(define-wajig-command ,command (pkg))))
- '(auto-install
-   available
-   build
-   build-depend
-   changelog
-   dependents
-   describe
-   detail
-   download
-   file-install
-   find-file
-   find-pkg
-   force
-   hold
-   install/dist
-   list-names
-   list-files
-   list-scripts
-   list-section
-   list-status
-   list-wide
-   move
-   news
-   package
-   readme
-   recommended
-   reconfigure
-   recursive
-   reload
-   repackage
-   restart
-   rpm2deb
-   rpminstall
-   rpmtodeb
-   search
-   show
-   source
-   start
-   status
-   status-match
-   status-search
-   stop
-   suggested
-   unhold
-   unofficial
-   update-alts
-   update-pci-ids
-   update-usb-ids
-   versions
-   whatis
-   whichpkg
+ '(
+   auto-install available build build-depend changelog dependents
+   describe detail download file-install find-file find-pkg force
+   hold install/dist list-names list-files list-scripts list-section
+   list-status list-wide move news package readme recommended
+   reconfigure recursive reload repackage restart rpm2deb rpminstall
+   rpmtodeb search show source start status status-match
+   status-search stop suggested unhold unofficial update-alts
+   update-pci-ids update-usb-ids versions whatis whichpkg
 
-   install
-   purge
-   purge-depend
-   purge-orphans
-   reinstall
-   remove
-   remove-depend
-   remove-orphans
-   upgrade))
+   install purge purge-depend purge-orphans reinstall remove
+   remove-depend remove-orphans upgrade
+   ))
 
 ;; Addtional/Redefined Commands
 ;; ----------------------------
@@ -408,42 +364,52 @@ pkg is the package name to operate on."
   (interactive)
   (wajig-do '("dpkg" "-l")))
 
+(defun wajig-upgrade-all ()
+  "$ apt-get upgrade	upgrade all packages"
+  (interactive)
+  (wajig-do '("sudo" "apt-get" "upgrade")))
+
 
 ;;; Wajig Mode
 
 (defvar wajig-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "E" 'wajig-edit-sources)
-    (define-key map "u" 'wajig-update)
-    (define-key map "s" 'wajig-search-by-name)
-    (define-key map "S" 'wajig-search)
-    (define-key map "c" 'wajig-commands)
-    (define-key map "h" 'wajig-help)
-    (define-key map "i" 'wajig-install)
-    (define-key map "R" 'wajig-remove)
-    (define-key map "L" 'wajig-list-files)
-    (define-key map "t" 'wajig-toupgrade)
-    (define-key map "U" 'wajig-upgrade)
-    (define-key map "w" 'wajig-whichpkg)
-    (define-key map "N" 'wajig-news)
-    (define-key map "m" 'wajig-manually)
-    (define-key map "o" 'wajig-show)
     (define-key map "C" 'wajig-auto-clean)
+    (define-key map "c" 'wajig-commands)
     (define-key map "d" 'wajig-source)
     (define-key map "D" 'wajig-source-download)
+    (define-key map "E" 'wajig-edit-sources)
+    (define-key map "h" 'wajig-help)
+    (define-key map "i" 'wajig-install)
+    (define-key map "L" 'wajig-list-files)
+    (define-key map "m" 'wajig-manually)
+    (define-key map "N" 'wajig-news)
+    (define-key map "o" 'wajig-show)
+    (define-key map "R" 'wajig-remove)
+    (define-key map "S" 'wajig-search)
+    (define-key map "s" 'wajig-search-by-name)
+    (define-key map "t" 'wajig-toupgrade)
+    (define-key map "u" 'wajig-update)
+    (define-key map "U" 'wajig-upgrade)
     (define-key map "" 'wajig-show-at-point)
+    (define-key map "w" 'wajig-whichpkg)
     ;; query status
     (define-key map (kbd "Q a") 'wajig-available)
-    (define-key map (kbd "Q c") 'wajig-changelog)
-    (define-key map (kbd "Q S") 'wajig-list-scripts)
-    (define-key map (kbd "Q r") 'wajig-readme)
-    (define-key map (kbd "Q l") 'wajig-list-log)
-    (define-key map (kbd "Q i") 'wajig-query-installed)
-    (define-key map (kbd "Q s") 'wajig-status)
-    (define-key map (kbd "Q n") 'wajig-non-free)
     (define-key map (kbd "Q b") 'wajig-bug)
+    (define-key map (kbd "Q c") 'wajig-changelog)
+    (define-key map (kbd "Q i") 'wajig-query-installed)
+    (define-key map (kbd "Q l") 'wajig-list-log)
+    (define-key map (kbd "Q n") 'wajig-non-free)
+    (define-key map (kbd "Q r") 'wajig-readme)
+    (define-key map (kbd "Q S") 'wajig-list-scripts)
+    (define-key map (kbd "Q s") 'wajig-status)
     ;; file operation
     (define-key map (kbd "F i") 'wajig-file-install)
+    ;; services control
+    (define-key map (kbd "V x") 'wajig-start)
+    (define-key map (kbd "V v") 'wajig-stop)
+    (define-key map (kbd "V r") 'wajig-restart)
+    (define-key map (kbd "V l") 'wajig-list-daemons)
     ;; cursor movement
     (define-key map "n" 'next-line)
     (define-key map "p" 'previous-line)
@@ -455,13 +421,25 @@ pkg is the package name to operate on."
     st)
   "Syntax table used while in `wajig-mode'.")
 
+(defvar wajig-font-lock-keywords
+  '(("\\(Package: \\)\\(.*\\)"
+     (1 font-lock-keyword-face nil t)
+     (2 font-lock-function-name-face nil t))
+    ("Conflicts: "
+     (0 font-lock-warning-face nil t))
+    ("^[a-zA-Z].*: "
+     (0 font-lock-keyword-face nil t)))
+  "Keywords to highlight in wajig mode.")
+
 (define-derived-mode wajig-mode nil "Wajig"
   "Major mode for wajig in debian.
 \\{wajig-mode-map}"
   (wajig-mode-help)
   (set-syntax-table wajig-mode-syntax-table)
+  (setq font-lock-defaults '(wajig-font-lock-keywords))
   (run-hooks 'wajig-mode-hook))
 
+;;;###autoload
 (defun wajig ()
   "Create a *wajig* buffer."
   (interactive)
