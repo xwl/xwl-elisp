fe19f9d006ba1a6b66a7bf7a29e4a516349a48c7
William Xu <william.xwl@gmail.com>
2007-01-28 09:36:00 +0000

    read-completer.el: Pack codes tightly.
    darcs-hash:20070128093638-cfa61-6e25a02ef93d16b8809e76f5c8d71c323db6f07f.gz

--- read-completer.el
+++ read-completer.el
@@ -4,7 +4,7 @@
 
 ;; Author: William Xu <william.xwl@gmail.com>
 ;; Version: 0.1
-;; Last updated: 2007/01/28 16:48:33
+;; Last updated: 2007/01/28 17:33:23
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -39,70 +39,24 @@
   :group 'read-completer)
 
 (defcustom read-completer-latex-tags
-      '( ;; sections
-        ("chapter"       . "\\chapter{.}")
-        ("paragraph"     . "\\paragraph{.}")
-        ("part"          . "\\part{.}")
-        ("section"       . "\\section{.}")
-        ("subsection"    . "\\subsection{.}")
-        ("subparagraph"  . "\\subparagraph{.}")
-        ("subsubsection" . "\\subsubsection{.}")
-        ;; fonts
-        ("textbf"     . "\\textbf{.}")
-        ("textsc"     . "\\textsc{.}")
-        ("emph"       . "\\emph{.}")
-        ("textsf"     . "\\textsf{.}")
-        ("textit"     . "\\textit{.}")
-        ("textmd"     . "\\textmd{.}")
-        ("textnormal" . "\\textnormal{.}")
-        ("textrm"     . "\\textrm{.}")
-        ("textsl"     . "\\textsl{.}")
-        ("texttt"     . "\\texttt{.}")
-        ("textup"     . "\\textup{.}")
-        ("mathcal"    . "\\mathcal{.}")  ; math fonts?
-        ("mathbf"     . "\\mathbf{.}")
-        ("mathsf"     . "\\mathsf{.}")
-        ("mathit"     . "\\mathit{.}")
-        ("mathnormal" . "\\mathnormal{.}")
-        ("mathrm"     . "\\mathrm{.}")
-        ("mathtt"     . "\\mathtt{.}")
-        ;; environments
-        ("abstract"        . "\\begin{abstract}\n.\n\\end{abstract}")
-        ("array"           . "\\begin{array}\n.\n\\end{array}")
-        ("center"          . "\\begin{center}\n.\n\\end{center}")
-        ("description"     . "\\begin{description}\n.\n\\end{description}")
-        ("displaymath"     . "\\begin{displaymath}\n.\n\\end{displaymath}")
-        ("document"        . "\\begin{document}\n.\n\\end{document}")
-        ("enumerate"       . "\\begin{enumerate}\n.\n\\end{enumerate}")
-        ("eqnarray"        . "\\begin{eqnarray}\n.\n\\end{eqnarray}")
-        ("eqnarray*"       . "\\begin{eqnarray*}\n.\n\\end{eqnarray*}")
-        ("equation"        . "\\begin{equation}\n.\n\\end{equation}")
-        ("figure"          . "\\begin{figure}\n.\n\\end{figure}")
-        ("figure*"         . "\\begin{figure*}\n.\n\\end{figure*}")
-        ("filecontents"    . "\\begin{filecontents}\n.\n\\end{filecontents}")
-        ("filecontents*"   . "\\begin{filecontents*}\n.\n\\end{filecontents*}")
-        ("flushleft"       . "\\begin{flushleft}\n.\n\\end{flushleft}")
-        ("flushright"      . "\\begin{flushright}\n.\n\\end{flushright}")
-        ("itemize"         . "\\begin{itemize}\n.\n\\end{itemize}")
-        ("list"            . "\\begin{list}\n.\n\\end{list}")
-        ("math"            . "\\begin{math}\n.\n\\end{math}")
-        ("minipage"        . "\\begin{minipage}\n.\n\\end{minipage}")
-        ("picture"         . "\\begin{picture}\n.\n\\end{picture}")
-        ("quotation"       . "\\begin{quotation}\n.\n\\end{quotation}")
-        ("quote"           . "\\begin{quote}\n.\n\\end{quote}")
-        ("sloppypar"       . "\\begin{sloppypar}\n.\n\\end{sloppypar}")
-        ("tabbing"         . "\\begin{tabbing}\n.\n\\end{tabbing}")
-        ("table"           . "\\begin{table}\n.\n\\end{table}")
-        ("table*"          . "\\begin{table*}\n.\n\\end{table*}")
-        ("tabular"         . "\\begin{tabular}\n.\n\\end{tabular}")
-        ("tabular*"        . "\\begin{tabular*}\n.\n\\end{tabular*}")
-        ("thebibliography" . "\\begin{thebibliography}\n.\n\\end{thebibliography}")
-        ("theindex"        . "\\begin{theindex}\n.\n\\end{theindex}")
-        ("titlepage"       . "\\begin{titlepage}\n.\n\\end{titlepage}")
-        ("trivlist"        . "\\begin{trivlist}\n.\n\\end{trivlist}")
-        ("verbatim"        . "\\begin{verbatim}\n.\n\\end{verbatim}")
-        ("verbatim*"       . "\\begin{verbatim*}\n.\n\\end{verbatim*}")
-        ("verse"           . "\\begin{verse}\n.\n\\end{verse}"))
+      `(,@(mapcar
+           (lambda (el)
+             `(,el . ,(format "\\%s{.}" el el)))
+           '("chapter" "paragraph" "part" "section" "subsection"
+             "subparagraph" "subsubsection" "textbf" "textsc" "emph"
+             "textsf" "textit" "textmd" "textnormal" "textrm" "textsl"
+             "texttt" "textup" "mathcal" "mathbf" "mathsf" "mathit"
+             "mathnormal" "mathrm" "mathtt"))
+        ,@(mapcar
+           (lambda (el)
+             `(,el . ,(format "\\begin{%s}\n.\n\\end{%s}" el el)))
+           '("abstract" "array" "center" "description" "displaymath"
+             "document" "enumerate" "eqnarray" "eqnarray*" "equation"
+             "figure" "figure*" "filecontents" "filecontents*" "flushleft"
+             "flushright" "itemize" "list" "math" "minipage" "picture"
+             "quotation" "quote" "sloppypar" "tabbing" "table" "table*"
+             "tabular" "tabular*" "thebibliography" "theindex" "titlepage"
+             "trivlist" "verbatim" "verbatim*" "verse")))
       "latex tags."
       :type 'symbol
       :group 'read-completer)
