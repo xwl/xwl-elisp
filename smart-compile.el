3bb1d1e249b5bc48955b26698a31c94c69f5eed9
William Xu <william.xwl@gmail.com>
2007-06-25 12:21:00 +0000

    smart-compile.el: When bin file `foo' for `foo.c' is not found. Prompt a
    shell-command explicitly.
    
    darcs-hash:20070625122152-cfa61-3eada9fa63ef5bb008e0fb1a34bad07cd6a938cd.gz

--- smart-compile.el
+++ smart-compile.el
@@ -227,16 +227,20 @@ See `smart-compile-table'."
 ;; Run shell command either synchronously or asynchronously with a
 ;; unique output buffer, whose window will be deleted automatically.
 (defun smart-compile-shell-command (cmd)
-  (if (string-match "&$" cmd)
-      (let ((buf (generate-new-buffer-name (concat "*" cmd "*"))))
-        (message cmd)
-        (shell-command cmd buf)
-        (delete-window (get-buffer-window buf)))
-    (shell-command cmd)))
+  (let ((ret nil))
+    (if (string-match "&$" cmd)
+        (let ((buf (generate-new-buffer-name (concat "*" cmd "*"))))
+          (message cmd)
+          (setq ret (shell-command cmd buf))
+          (delete-window (get-buffer-window buf)))
+      (setq ret (shell-command cmd)))
+    ret))
 
 (defun smart-compile-run1 ()
   (cond ((stringp smart-compile-run-handler)
-         (smart-compile-shell-command smart-compile-run-handler))
+         (unless (zerop (smart-compile-shell-command smart-compile-run-handler))
+           (call-interactively 'shell-command)
+           (setq smart-compile-run-handler (car shell-command-history))))
         (t
          (funcall smart-compile-run-handler))))
 
