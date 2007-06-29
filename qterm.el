bfc63e2a5daaa918f75961ec3030f08a6f2b9ae9
William Xu <william.xwl@gmail.com>
2007-06-29 12:09:00 +0000

    Initial commit.
    darcs-hash:20070629120906-cfa61-c8fa6ccabcf298b6d2abefa47d6a501be01d0656.gz

---
+++ qterm.el
@@ -0,0 +1,72 @@
+;;; qterm.el --- qterm replying editor hook
+
+;; Copyright (C) 2007 William Xu
+
+;; Author: William Xu <william.xwl@gmail.com>
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301 USA
+
+;; Put this file into your load-path and the following into your
+;; ~/.emacs:
+;;           (require 'qterm)
+
+;;; Code:
+
+(defvar qterm-faces '("朗声" "鬼叫" "喃喃" "轻声" "一声大喝" "大叫" 
+                      "柔柔" "哭着" "大吼"))
+
+(defvar qterm-log-file "/tmp/qterm.log")
+
+(defun qterm-reply-hook ()
+  (when (string= (buffer-file-name) qterm-log-file)
+    (let ((inhibit-read-only nil))
+      (less-minor-mode-off)
+      ;; quote
+      (goto-char (point-max))
+      (delete-blank-lines)
+      (goto-char (point-min))
+      (replace-regexp "^" "> ")
+      ;; author title
+      (let ((author "")
+            (face (nth (random (length qterm-faces)) qterm-faces)))
+        (goto-char (point-min))
+        (re-search-forward "[a-zA-Z0-9]+.*)"
+                           (save-excursion (end-of-line)
+                                           (point))
+                           t
+                           1)
+        (setq author (match-string 0))
+        (goto-char (point-min))
+        (kill-line 4)
+        (insert (format "%s[ %s %s道: ]%s\n\n" 
+                        ansit-color-yellow author face ansit-color-close)))
+      ;; qmd (optionally uncomment the following if you have fortune-zh
+      ;; installed, with which you will get random poems qmd. ;-)
+      (goto-char (point-max))
+      (insert 
+       (format "\n\n%s--\n%s%s"
+               ansit-color-magenta
+               (ansi-color-filter-apply
+                (shell-command-to-string "fortune-zh"))
+               ansit-color-close))
+      (goto-char (point-min)))))
+
+(add-hook 'find-file-hook 'qterm-reply-hook)
+
+(provide 'qterm)
+
+;;; qterm.el ends here
