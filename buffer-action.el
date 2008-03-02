ac67c32b81a2846453dec81290d9c25dc40489d8
William Xu <william.xwl@gmail.com>
2008-03-02 05:15:00 +0000

    Update GPLv2 to GPLv3.
    darcs-hash:20080302051557-cfa61-6969b85076ae5d8d35f6aa976c832e4dfcabff00.gz

--- buffer-action.el
+++ buffer-action.el
@@ -1,4 +1,4 @@
-;;; buffer-action.el --- Perform actions(compile, etc) in buffer based on mode/filename
+;;; buffer-action.el --- Perform actions(compile/run, etc) in buffer based on mode/filename
 
 ;; Copyright (C) 2005, 2007, 2008 William Xu
 
@@ -6,20 +6,20 @@
 ;; Version: 3.0
 ;; Url: http://williamxu.net9.org/ref/buffer-action.el
 
-;; This file is free software; you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
-;; This file is distributed in the hope that it will be useful,
+;; EMMS is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
+;; along with EMMS; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
