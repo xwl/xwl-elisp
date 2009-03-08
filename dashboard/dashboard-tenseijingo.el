be1ff01d2e66c912ea8f649a3d7932f5ca19f2d5
William Xu <william.xwl@gmail.com>
2009-03-08 23:25:43 +0800

    Use external tool `w3m' for dumping html to plain texts.

--- dashboard/dashboard-tenseijingo.el
+++ dashboard/dashboard-tenseijingo.el
@@ -1,6 +1,6 @@
 ;;; dashboard-tenseijingo.el --- tenseijingo colum from www.asahi.com
 
-;; Copyright (C) 2008 William Xu
+;; Copyright (C) 2008, 2009 William Xu
 
 ;; Author: William Xu <william.xwl@gmail.com>
 ;; Version: 0.1
@@ -38,33 +38,57 @@
 (defun dashboard-tenseijingo ()
   "Get tenseijingo colum from http://www.asahi.com."
   (let ((url "http://www.asahi.com/paper/column.html"))
-    (if (marker-position dashboard-tenseijingo-upper-left-marker)
-        (url-retrieve url 'dashboard-tenseijingo-callback)
-      (with-current-buffer (url-retrieve-synchronously url)
-        (funcall 'dashboard-tenseijingo-callback)))))
+    ;; (if (marker-position dashboard-tenseijingo-upper-left-marker)
+    ;;     (url-retrieve url 'dashboard-tenseijingo-callback)
+    ;;   (with-current-buffer (url-retrieve-synchronously url)
+    ;;     (funcall 'dashboard-tenseijingo-callback)))))
+    (dashboard-tenseijingo-update url)))
 
-(defun dashboard-tenseijingo-callback (&optional status)
-  (when (eq :error (car status))
-    (error "dashboard-tenseijingo-callback: %S" status))
-  (url-html-decode-buffer)
-  ;; (switch-to-buffer (current-buffer))
-  (goto-char (point-min))
-  (let ((date "")
-        (content ""))
-    (when (re-search-forward "<h1 id=\"genre\">.*</h1>" nil t 1)
-      (re-search-forward "<p id=\"date\">\\(.*\\)</p>" nil t 1)
-      (setq date (match-string 1))
-      (search-forward "<div class=\"kiji\">" nil t 1)
-      (search-forward "<p><p>" nil t 1)
-      (skip-chars-forward "[[:blank:]\n]")
-      (let ((start (point)))
-        (search-forward "</p>" nil t 1)
-        (backward-char (1+ (length "</p>")))
-        (setq content (buffer-substring-no-properties start (point)))))
-    (kill-buffer (current-buffer))
-    (dashboard-tenseijingo-format content date)))
 
-(defun dashboard-tenseijingo-format (content date)
+(defun dashboard-tenseijingo-update (url)
+  "Use external tool w3m."
+  (let (content date)
+    ;; (with-current-buffer "a"
+    (with-temp-buffer
+      (shell-command (concat "w3m -dump " url) t)
+      (goto-char (point-min))
+      (re-search-forward "[0-9]\\{4\\}年[0-9]月[0-9]日" nil t 1)
+      (setq date (delete-and-extract-region (line-beginning-position)
+                                            (line-end-position)))
+      (search-forward "印刷" nil t 1)
+      (goto-char (line-end-position))
+      (skip-chars-forward "[:space:]")
+      (setq content
+            (delete-and-extract-region (point)
+                                       (progn (forward-paragraph)
+                                              (skip-chars-backward "[:space:]")
+                                              (point))))
+      (kill-buffer (current-buffer))
+      (dashboard-tenseijingo-format url content date))))
+
+;; (defun dashboard-tenseijingo-callback-by-url (&optional status)
+;;   "Use emacs's url lib."
+;;   (when (eq :error (car status))
+;;     (error "dashboard-tenseijingo-callback: %S" status))
+;;   (url-html-decode-buffer)
+;;   (switch-to-buffer (current-buffer))
+;;   (goto-char (point-min))
+;;   (let ((date "")
+;;         (content ""))
+;;     (when (re-search-forward "<h1 id=\"genre\">.*</h1>" nil t 1)
+;;       (re-search-forward "<p id=\"date\">\\(.*\\)</p>" nil t 1)
+;;       (setq date (match-string 1))
+;;       (search-forward "<div class=\"kiji\">" nil t 1)
+;;       (search-forward "<p><p>" nil t 1)
+;;       (skip-chars-forward "[[:blank:]\n]")
+;;       (let ((start (point)))
+;;         (search-forward "</p>" nil t 1)
+;;         (backward-char (1+ (length "</p>")))
+;;         (setq content (buffer-substring-no-properties start (point)))))
+;;     (kill-buffer (current-buffer))
+;;     (dashboard-tenseijingo-format content date)))
+
+(defun dashboard-tenseijingo-format (url content date)
   "Called by `dashboard-tenseijingo-callback'."
   (with-temp-buffer
     (let ((width 0))
@@ -74,8 +98,9 @@
       (goto-char (point-max))
       (setq width (current-column))
       (insert "\n\n")
-      (let ((title (concat "天声人語 " date)))
-        (insert (dashboard-padding title width)))
+      (insert (dashboard-padding (concat "天声人語 " date) width))
+      (insert "\n")
+      (insert (dashboard-padding url width))
       (dashboard-insert-as-rectangle 'tenseijingo (buffer-string)))))
 
 
